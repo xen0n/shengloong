2ded4468e3ab399b74c627ccdbf7163eb61bb0a4
WANG Xuerui <xen0n@gentoo.org>
2022-05-12 14:59:34 +0800

    test: add big-endian case to e2e-ignores

--- tests/e2e-ignores/regen.sh
+++ tests/e2e-ignores/regen.sh
@@ -2,14 +2,18 @@
 
 CC_RISCV64="${CC_RISCV64:-riscv64-unknown-linux-gnu-gcc}"
 STRIP_RISCV64="${STRIP_RISCV64:-riscv64-unknown-linux-gnu-strip}"
-CC_MIPS="${CC_MIPS:-mips64el-unknown-linux-gnu-gcc}"
-STRIP_MIPS="${STRIP_MIPS:-mips64el-unknown-linux-gnu-strip}"
+CC_MIPS64EB="${CC_MIPS64EB:-mips64-unknown-linux-gnu-gcc}"
+STRIP_MIPS64EB="${STRIP_MIPS64EB:-mips64-unknown-linux-gnu-strip}"
+CC_MIPS64EL="${CC_MIPS64EL:-mips64el-unknown-linux-gnu-gcc}"
+STRIP_MIPS64EL="${STRIP_MIPS64EL:-mips64el-unknown-linux-gnu-strip}"
 
-"$CC_MIPS" -O3 -mabi=32 -march=mips64r2 -EL -o bin/hello.mipso32 ./hello.c
-"$STRIP_MIPS" bin/hello.mipso32
-"$CC_MIPS" -O3 -mabi=n32 -march=mips64r2 -EL -o bin/hello.mipsn32 ./hello.c
-"$STRIP_MIPS" bin/hello.mipsn32
-"$CC_MIPS" -O3 -mabi=64 -march=mips64r2 -EL -o bin/hello.mipsn64 ./hello.c
-"$STRIP_MIPS" bin/hello.mipsn64
+"$CC_MIPS64EB" -O3 -mabi=64 -march=mips64r2 -EB -o bin/hello.mipsn64eb ./hello.c
+"$STRIP_MIPS64EB" bin/hello.mipsn64eb
+"$CC_MIPS64EL" -O3 -mabi=32 -march=mips64r2 -EL -o bin/hello.mipso32 ./hello.c
+"$STRIP_MIPS64EL" bin/hello.mipso32
+"$CC_MIPS64EL" -O3 -mabi=n32 -march=mips64r2 -EL -o bin/hello.mipsn32 ./hello.c
+"$STRIP_MIPS64EL" bin/hello.mipsn32
+"$CC_MIPS64EL" -O3 -mabi=64 -march=mips64r2 -EL -o bin/hello.mipsn64 ./hello.c
+"$STRIP_MIPS64EL" bin/hello.mipsn64
 "$CC_RISCV64" -O3 -mabi=lp64d -march=rv64gc -o bin/hello.rv64 ./hello.c
 "$STRIP_RISCV64" bin/hello.rv64
--- tests/e2e-ignores/runtest.sh
+++ tests/e2e-ignores/runtest.sh
@@ -33,6 +33,7 @@ echo
 info 'checksumming -- all should stay intact'
 assert_sha256sum e07ea1c69ab3f57fb4bf681d3ff8d1aec31e838af27eb2b5fbcadc1705753377 "$workdir/hello.mipsn32"
 assert_sha256sum b1d625d4f30eae709893d13c11f0af47934b51dece6036715e762a487c7c0b3c "$workdir/hello.mipsn64"
+assert_sha256sum 3750b274cd9753818b7077d11b490649a35c35a926c5c77d9737a052528b1f77 "$workdir/hello.mipsn64eb"
 assert_sha256sum 6a45f3fdacd2c13905ec74f6f5ce4dfdfa35ce2a44eb8597143099363d5aa3bf "$workdir/hello.mipso32"
 assert_sha256sum 780a51e63a63e9beb0dd6483fd6b2cbd826d42f19aa000be727710e76e6ecca2 "$workdir/hello.rv64"
 
